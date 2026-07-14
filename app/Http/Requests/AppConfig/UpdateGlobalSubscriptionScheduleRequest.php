4f195d6e774f3c4cf885962df16824fe2a0ef4e1
alex21x <afernandez@truenorh.pr>
2026-07-14 13:10:15 -0500

    20260714_ControlSuscripciones

--- app/Http/Requests/AppConfig/UpdateGlobalSubscriptionScheduleRequest.php
+++ app/Http/Requests/AppConfig/UpdateGlobalSubscriptionScheduleRequest.php
@@ -8,7 +8,7 @@ class UpdateGlobalSubscriptionScheduleRequest extends AppConfigFormRequest
     {
         return [
             'company_subscription_alert_frequency' => 'required|string|in:DAILY,WEEKLY,MONTHLY',
-            'company_subscription_alert_time' => 'required|string|regex:/^(?:[01]?\d|2[0-3]):[0-5]\d$/',
+            'company_subscription_alert_time' => 'required|string|date_format:H:i',
             'company_subscription_weekly_digest_day' => 'required|integer|min:1|max:7',
             'company_subscription_monthly_digest_day' => 'required|integer|min:1|max:28',
         ];
